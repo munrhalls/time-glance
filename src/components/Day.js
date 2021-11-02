76e409199a45488c860710beb7a86ff4faa13dac
munrhalls <52234492+munrhalls@users.noreply.github.com>
2021-11-02 03:49:30 +0100

    refactor

--- src/components/Day.js
+++ src/components/Day.js
@@ -6,36 +6,23 @@ function Day({ updateCards, dayCards }) {
   const handleDrop = (e) => {
     e.preventDefault();
     const card = JSON.parse(e.dataTransfer.getData("card")).timeCard;
-    // console.log(card)
-    const getStartHour = () => {
-      const dayWidth = dayRef.current.clientWidth;
-      const dropCoordX = e.clientX;
-      const hourWidth = dayWidth / 24;
-      const numStartHour = Number(Math.floor(dropCoordX / hourWidth));
-      return numStartHour;
-    };
-    card.numStartHour = getStartHour();
+    const hourWidth = dayRef.current.clientWidth / 24;
+    card.numStartHour = Number(Math.floor(e.clientX / hourWidth));
     updateCards(e, card);
   };
   const handleDragOver = (e) => {
     e.preventDefault();
   };
-
   const numHourUnit = 2;
-  const dayHeight = numHourUnit * 24;
-  const dayWidth = numHourUnit * 24;
-  const hourMarks = [...Array(7).keys()]
-    .map((i) => i * 4)
-    .map((mark) => {
-      return mark;
-    });
 
   return (
     <div
       ref={dayRef}
       onDrop={handleDrop}
       onDragOver={handleDragOver}
-      className={`h-${dayHeight} w-${dayWidth} mt-40 border-black border-2 rounded-2x1 flex flex-col`}
+      className={`h-${numHourUnit * 24} w-${
+        numHourUnit * 24
+      } mt-40 border-black border-2 rounded-2x1 flex flex-col`}
     >
       <div className="h-5/6 relative">
         {dayCards.map((dayCard) => (
@@ -47,14 +34,16 @@ function Day({ updateCards, dayCards }) {
         ))}
       </div>
       <div className="h-1/6 bg-black text-white flex">
-        {hourMarks.map((mark) => (
-          <span
-            key={mark}
-            className="text-xs font-extrabold flex-1 flex justify-center items-center"
-          >
-            {mark}
-          </span>
-        ))}
+        {[...Array(7).keys()]
+          .map((i) => i * 4)
+          .map((mark) => (
+            <span
+              key={mark}
+              className="text-xs font-extrabold flex-1 flex justify-center items-center"
+            >
+              {mark}
+            </span>
+          ))}
       </div>
     </div>
   );
