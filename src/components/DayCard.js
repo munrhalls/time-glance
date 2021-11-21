9b4ea223aae07d4be13f77aa7a31fd809f128d35
John <jpilik@fream.pl>
2021-11-21 12:37:56 +0100

    daycard looks better

--- src/components/DayCard.js
+++ src/components/DayCard.js
@@ -5,13 +5,13 @@ function DayCard({ dayCard }) {
 
   return (
     <div className={`bg-${dayCard.bgColor}-900 absolute text-black text-2xl bg-black border-black flex justify-center content-center`} 
-    style={{height: '90%', marginTop: '1.25%', marginLeft: `${dayCard.startHour/2}rem`, width: `${dayCard.duration/2}rem`, borderRadius: '4px'}}>
+    style={{height: '87.5%', marginTop: '2.25%', marginLeft: `${dayCard.startHour/2}rem`, width: `${dayCard.duration/2}rem`, borderRadius: '4px'}}>
       {dayCard.duration}
     </div>
   )
 }
 
-// DayCard.propTypes = {
+// DayCard.propTypes = {  
 
 // }
 
