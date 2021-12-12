14237cefec4c70ca00b7ba3de6cd15bd92cd8f42
John <jpilik@fream.pl>
2021-12-12 22:24:54 +0100

    AWESOME DRAGOVER EFFECT !!!

--- src/components/Dragover.js
+++ src/components/Dragover.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Dragover = () => {
-    
+    const highlight = [1, 2, 3, 4];
+    let pos;
+
+    // const [box, setBox] = useState([...highlight]);
+    const [location, setLocation] = useState(pos)
+    const getCoords = (e) => {
+        setLocation(e.clientX)
+    }
+
+    console.log(location)
     return (
-        <div className="Dragover">
-           Dragover. 
+        <div className="Dragover" onMouseMove={getCoords}>
+            <div style={{
+                position: 'absolute',
+                background: '#000',
+                width: '4rem',
+                height: '4rem',
+                left: `${location}px`,
+                top: '8rem'
+            }}>box</div>
         </div>
     )
 }
