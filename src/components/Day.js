310b395fc2f3e468033a4ea22db47538d9c2e234
John <jpilik@fream.pl>
2021-12-14 14:16:11 +0100

    css, fix bugs

--- src/components/Day.js
+++ src/components/Day.js
@@ -1,20 +1,33 @@
 import React from 'react'
 
 export const Day = ({ day }) => {
-    const hourMarks = [0, 4, 8, 12, 16, 20, 24];
-    const hourColumns = [...Array(24).keys()];
+    const hourMarks = [4, 8, 12, 16, 20, 24];
+    const HourRows = [...Array(24).keys()];
+    const calcMargin = (hourMark) => {
+        if (hourMark === 24) {
+            return 0.25
+        }
+    }
 
     return (
         <div className="Day">
             <div className="DayTitle">
                 {day}
             </div>
-            <div className="HourColumns">
-                {hourColumns.map(hourColumn => { return <div className="HourColumn" key={hourColumn}>.</div> })}
+            <div className="DayHours">
+                <div className="HourMarks">
+                    {hourMarks.map((hourMark, index) => {
+                        return <div className="HourMark" key={hourMark}
+                            style={{ marginTop: `${calcMargin(hourMark)}` }}> {hourMark}</div>
+                    })}
+                </div>
+                <div className="HourRows">
+                    {HourRows.map(HourRow => {
+                        return <div className="HourRow" key={HourRow}>
+                        </div>
+                    })}
+                </div>
             </div>
-            <div className="HourMarks">
-                {hourMarks.map(hourMark => { return <span className="HourMark" key={hourMark}>{hourMark}</span> })}
-            </div>
-        </div>
+        </div >
     )
 }
