dc644edfed5b6d1dd83eb7d205c3d2521d27f189
John <jpilik@fream.pl>
2021-12-20 14:25:57 +0100

    adding card works

--- src/components/TimeDeck.js
+++ src/components/TimeDeck.js
@@ -5,6 +5,7 @@ import { Button } from "./Button";
 export const TimeDeck = ({ timeDeck }) => {
   const [timeCards, setTimeCards] = useState([{}]);
   const addTimeCard = () => {
+    console.log("Add time card.");
     const newTimeCard = {
       id: timeCards.length + 1,
       color: "blue",
@@ -18,7 +19,7 @@ export const TimeDeck = ({ timeDeck }) => {
   return (
     <div className="TimeDeck" style={{ position: "relative" }}>
       {/* <div className="TimeDeckBg" style={{ backgroundColor: `${timeDeck.color}`, opacity: '0.51', position: 'absolute', top: '0', bottom: '0', left: '0', right: '0' }}></div> */}
-      <Button type="add" label="New Card." onClick={addTimeCard}></Button>
+      <Button type="add" label="New Card." handlerFunc={addTimeCard} />
       {timeCards.map((timeCard) => {
         return <TimeCard timeCard={timeCard} key={timeCard.id + "1"} />;
       })}
