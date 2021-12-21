5014a78ae883b7fbfdfc34604477c763288a7312
John <jpilik@fream.pl>
2021-12-21 22:22:41 +0100

    data structure

--- src/components/EditTimeCardsMenu.js
+++ src/components/EditTimeCardsMenu.js
@@ -2,18 +2,33 @@ import React from "react";
 import { Button } from "./Button";
 
 export const EditTimeCardsMenu = ({ editTimeCards, closeEditMenu }) => {
-  const nums = [...Array(24).keys()].map((el) => el + 1);
-
+  const numRows = [0, 4, 8, 12, 16, 20];
+  const nums = [];
+  numRows.forEach((row) => {
+    const numsForRow = [];
+    for (let i = row; i < row + 4; i++) {
+      numsForRow.push(i + 1);
+    }
+    nums.push(numsForRow);
+  });
+  console.log(nums);
   return (
     <div className="EditTimeCardsMenu">
-      {nums.map((num) => (
+      {numRows.map((row) => {
+        return (
+          <div className="RowNumBtns" style={{ color: "#fff" }}>
+            Num Row {row}
+          </div>
+        );
+      })}
+      {/* {nums.map((num) => (
         <Button
           type="num"
           label={num.toString()}
           handlerFunc={() => editTimeCards(num)}
           key={num + Math.random()}
         />
-      ))}
+      ))} */}
       <Button type="exit" label="Close." handlerFunc={closeEditMenu} />
     </div>
   );
