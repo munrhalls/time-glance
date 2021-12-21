46f6bc03ebfd9b8ce795ca1c771731e827473bfd
John <jpilik@fream.pl>
2021-12-21 23:03:13 +0100

    num btns for edit mode. time cards menu. btns.

--- src/components/EditTimeCardsMenu.js
+++ src/components/EditTimeCardsMenu.js
@@ -2,9 +2,8 @@ import React from "react";
 import { Button } from "./Button";
 
 export const EditTimeCardsMenu = ({ editTimeCards, closeEditMenu }) => {
-  const numRows = [0, 4, 8, 12, 16, 20];
   const nums = [];
-  numRows.forEach((row) => {
+  [0, 4, 8, 12, 16, 20].forEach((row) => {
     const numsForRow = [];
     for (let i = row; i < row + 4; i++) {
       numsForRow.push(i + 1);
@@ -12,38 +11,24 @@ export const EditTimeCardsMenu = ({ editTimeCards, closeEditMenu }) => {
     nums.push(numsForRow);
   });
 
-  console.log(nums);
   return (
     <div className="EditTimeCardsMenu">
       {nums.map((row) => {
         return (
-          <div
-            className="NumBtnRow"
-            style={{ color: "#fff", display: "flex" }}
-            key={row + Math.random()}
-          >
+          <div className="NumBtnsRow" key={row + Math.random()}>
             {row.map((num) => {
               return (
-                <div
-                  className="NumBtnCol"
-                  style={{ display: "flex", border: "1px solid #fff" }}
+                <Button
+                  type="num"
+                  label={num.toString()}
+                  handlerFunc={() => editTimeCards(num)}
                   key={num + Math.random()}
-                >
-                  {num}
-                </div>
+                />
               );
             })}
           </div>
         );
       })}
-      {/* {nums.map((num) => (
-        <Button
-          type="num"
-          label={num.toString()}
-          handlerFunc={() => editTimeCards(num)}
-          key={num + Math.random()}
-        />
-      ))} */}
       <Button type="exit" label="Close." handlerFunc={closeEditMenu} />
     </div>
   );
