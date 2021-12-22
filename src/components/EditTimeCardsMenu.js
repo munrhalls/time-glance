db84a536196639bec667e46410633eaba1ab9d6c
John <jpilik@fream.pl>
2021-12-22 20:56:57 +0100

    edit time cards menu

--- src/components/EditTimeCardsMenu.js
+++ src/components/EditTimeCardsMenu.js
@@ -2,33 +2,23 @@ import React from "react";
 import { Button } from "./Button";
 
 export const EditTimeCardsMenu = ({ editTimeCards, closeEditMenu }) => {
-  const nums = [];
-  [1, 7, 13, 19].forEach((row) => {
-    const numsForRow = [];
-    for (let i = row; i < row + 6; i++) {
-      numsForRow.push(i);
-    }
-    nums.push(numsForRow);
-  });
-
+  const nums = [1, 2, 3, 4, 5, 6];
   return (
     <div className="EditTimeCardsMenu">
-      {nums.map((row) => {
-        return (
-          <div className="NumBtnsRow" key={row + Math.random()}>
-            {row.map((num) => {
-              return (
-                <Button
-                  type="num"
-                  label={num.toString()}
-                  handlerFunc={() => editTimeCards(num)}
-                  key={num + Math.random()}
-                />
-              );
-            })}
-          </div>
-        );
-      })}
+      <div className="NumBtnsArrLeft">arr left</div>
+      <div className="NumBtnsRow">
+        {nums.map((num) => {
+          return (
+            <Button
+              type="num"
+              label={num}
+              handlerFunc={() => editTimeCards(1)}
+            />
+          );
+        })}
+      </div>
+      <div className="NumBtnsArrRight">arr right</div>
+
       <Button type="exit" label="Close." handlerFunc={closeEditMenu} />
     </div>
   );
