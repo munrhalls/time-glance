1975ea649983f1894e58b197bb993739e1964eb1
John <jpilik@fream.pl>
2021-12-22 22:15:13 +0100

    nums menu btns work

--- src/components/EditTimeCardsMenu.js
+++ src/components/EditTimeCardsMenu.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "./Button";
 
 export const EditTimeCardsMenu = ({ editTimeCards, closeEditMenu }) => {
-  const nums = [1, 2, 3, 4, 5, 6];
+  const [nums, setNums] = useState([1, 2, 3, 4, 5, 6]);
+  const scrollRight = () => {
+    setNums((nums) => {
+      return nums.map((num) => (num + 6 <= 24 ? (num += 6) : num));
+    });
+  };
+  const scrollLeft = () => {
+    setNums((nums) => {
+      return nums.map((num) => (num - 6 > 0 ? (num -= 6) : num));
+    });
+  };
+
   return (
     <div className="EditTimeCardsMenu">
-      <Button type="scrollLeft"></Button>
+      <Button type="scrollLeft" handlerFunc={scrollLeft}></Button>
       <div className="NumBtnsRow">
         {nums.map((num) => {
           return (
@@ -17,7 +28,7 @@ export const EditTimeCardsMenu = ({ editTimeCards, closeEditMenu }) => {
           );
         })}
       </div>
-      <Button type="scrollRight"></Button>
+      <Button type="scrollRight" handlerFunc={scrollRight}></Button>
 
       <Button type="exit" label="Close." handlerFunc={closeEditMenu} />
     </div>
